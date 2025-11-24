5339bb58e83cca166979a3fe74dc6face032dd26
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-24 08:53:23 -0300

    feat(migration product table): Migration da tabela de produtos alterada

---
+++ database/migrations/2025_10_31_115537_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('size')->nullable();
+            $table->string('color')->nullable();
+            $table->decimal('price', 10, 2)->default(0);
+            $table->integer('stock')->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
