b46c79ed130eb5b77c58f322ff0439591349d6f3
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-28 15:48:22 -0300

    feat(tailwind.config.js): Novas classes de cores pré definidas no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                beige: {
+                    50: '#FCF9F6',
+                    100: '#FBF6F0',
+                    200: '#F7EEE2',
+                    300: '#F3E6D3',
+                    400: '#E9D3B8',
+                    500: '#D9B996',
+                    600: '#BFA076',
+                    700: '#997A57',
+                    800: '#7A5E42',
+                    900: '#5E452F',
+                },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
