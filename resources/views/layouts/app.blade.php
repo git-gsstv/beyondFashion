d691e81fb0d66198fc1b4489230811d23f76658e
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-28 15:51:54 -0300

    feat(nav e app.blade): estilizações padrões do breeze modificadas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,34 +3,36 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+
+    <body class="font-sans antialiased bg-beige-400 text-beige-900">
+
+        <div class="min-h-screen">
+
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="shadow-sm bg-beige-300">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        <h2 class="font-semibold text-xl text-beige-900 leading-tight">
+                            {{ $header }}
+                        </h2>
                     </div>
                 </header>
             @endisset
 
-            <!-- Page Content -->
-            <main>
+            <main class="py-6">
                 {{ $slot }}
             </main>
+
         </div>
+
     </body>
 </html>
