ece87c4550e8774c11d2188cb2c1288ac3020564
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-28 18:29:32 -0300

    feat(view): cria a view para edição de categorias

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-beige-900 leading-tight">
+            {{ __('Editar Categoria') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-beige-200 overflow-hidden shadow-md shadow-beige-500/50 sm:rounded-lg p-6">
+                
+                {{-- Presumindo que o Controller passa a variável $category --}}
+                @include('categories.form', ['category' => $category])
+                
+            </div>
+        </div>
+    </div>
+</x-app-layout>
