6a541ebb3bc0edaad3e00acc35d624bed7d12c8a
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-28 18:30:13 -0300

    feat(routes): adiciona rota de recurso para categorias

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', function () {
@@ -16,6 +17,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
 
     Route::resource('products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
