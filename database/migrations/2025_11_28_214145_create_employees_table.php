15cd3b1288d053a282f95ea3c18fcd3d2a2bb9c8
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-28 20:05:14 -0300

    feat(migration): cria a migração para a tabela de funcionários

---
+++ database/migrations/2025_11_28_214145_create_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 150);
+            $table->string('cargo', 100);
+            $table->enum('turno', ['Manhã', 'Tarde', 'Noite']); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
