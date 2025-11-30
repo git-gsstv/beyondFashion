b2f14fa7addddc07f54b0dd29f875c647e29b2c3
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-11-29 23:01:16 -0300

    feat(view): atualiza o layouy do breeze com traduções e melhorias na interface

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,22 +7,23 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        {{-- Fundo da página mais escuro: de bg-beige-200 para **bg-beige-300** --}}
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-beige-300">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    {{-- Logo em cor de contraste: Mantido text-beige-700 --}}
+                    <x-application-logo class="w-20 h-20 fill-current text-beige-700" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            {{-- Fundo do card mais claro (para contraste): de bg-beige-50 para **bg-beige-100** --}}
+            <div class="w-full sm:max-w-md mt-6 px-6 py-8 bg-beige-100 shadow-xl overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
